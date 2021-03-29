a472ace5dfd4df327548671e18981668dbe8997a
Wescley Serrão <wescleysp@gmail.com>
2021-03-29 11:09:54 -0300

    :tada: challenge  finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
+import { useContext } from 'react';
+
+import '../styles/Content.scss';
+
+import { MovieCard } from '../components/MovieCard';
+import MovieContext from '../context/moviesContext'
+
 export function Content() {
-  // Complete aqui
+  
+  const { movies, selectedGenre } = useContext(MovieContext);
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
+import { useContext } from 'react';
+
+import '../styles/SideBar.scss';
+
+import MovieContext from '../context/moviesContext'
+import { Button } from '../components/Button';
+
 export function SideBar() {
-  // Complete aqui
+
+  const { genres, selectedGenreId, handleClickButton } = useContext(MovieContext);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
